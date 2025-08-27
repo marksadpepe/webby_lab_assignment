44882845de18e61c6a1068be05315a3621ac6790
Mykhailo Voznesenskyi <mykhailo.voznesenskyi@amedia.ua>
2025-08-27 13:22:13 +0300

    feat: added models

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,23 @@
-import {Sequelize} from "sequelize";
+import {Sequelize} from "sequelize-typescript";
 import {config} from "../config/config";
 import {Client} from "pg"
+import { MovieModel } from "../models/movie.model";
+import { ActorModel } from "../models/actor.model";
+import { MovieActorModel } from "../models/movie-actor.model";
 
 const {database: {databaseName, host, port, password, username, logging}} = config
 
-export const sequelize = new Sequelize(databaseName, username, password, {host, port, dialect: "postgres", logging})
+export const sequelize = new Sequelize({
+  dialect: "postgres",
+  host,
+  port,
+  username,
+  password,
+  database: databaseName,
+  logging
+})
+
+sequelize.addModels([MovieModel, ActorModel, MovieActorModel])
 
 export async function ensureDatabaseExists() {
   const client = new Client({host, port, user: username, password, database: "postgres"})
