038dfb531b46cf3961219e637bc1c4f5149a982e
Mykhailo Voznesenskyi <mykhailo.voznesenskyi@amedia.ua>
2025-08-27 13:22:39 +0300

    feat: init backend server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import 'dotenv/config';
 
+import * as express from "express";
+import {config} from "./config/config";
 import {connectDatabase, ensureDatabaseExists} from "./db/db"
+import {movieRouter as MovieRouter} from "./routers/moview.router";
+import {httpErrorMiddleware} from "./middlewares/http-exception.middleware";
+
+const {appPort} = config;
+
+const app = express();
+
+app.use(express.json())
+app.use("/api/v1", MovieRouter);
+app.use(httpErrorMiddleware)
+
 
 const startApp = async () => {
   try {
     await ensureDatabaseExists();
 
     await connectDatabase();
+
+    app.listen(appPort, () => {
+      console.log(`\nServer is listening on ${appPort} port`);
+    })
   } catch (err) {
     console.error(err)
 
